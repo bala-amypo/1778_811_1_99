77a785ed465e64642608377d55cf9a0c3da1ec02
bala-amypo <bala@amypo.in>
2025-12-18 06:14:28 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/RoleServiceImpl.java
@@ -1,4 +1,17 @@
 package com.demo.service.impl;
 imnport com.example.demo.entity.Role;
 import com.example.demo.repository.RoleRepository;
-import com.example.demo.service.RoleRepository;
+import com.example.demo.service.RoleService;
+import org.framework.stereotype.Service;
+@Service
+@Column(unique=true)
+public class RoleServiceImpl implements RoleService{
+    private final RoleRepository roleRepository;
+    public RoleServiceImpl(RoleRepository roleRepository){
+        this.roleRepository=roleRepository;
+    }
+    @Override
+    public createRole(String rolename){
+        return roleRepository.findByName(rolename).orElse(()=>roleRepository.save(new Role(roleName)));
+    }
+}
