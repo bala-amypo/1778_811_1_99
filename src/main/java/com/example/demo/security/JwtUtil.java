7915d69e7a6112738d870b98590d6e53766d4742
bala-amypo <bala@amypo.in>
2025-12-20 19:35:34 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,5 +1,9 @@
 package com.example.demo.security;
 
+import org.springframework.stereotype.Component;
+
 @Component
 public class JwtUtil {
+    // Dummy placeholder class
+    // Intentionally left empty
 }
