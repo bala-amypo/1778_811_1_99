92a3c6a451b20e61f3cbc4b8e317fb266859fbfa
bala-amypo <bala@amypo.in>
2025-12-23 08:55:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,5 +1,5 @@
 package com.example.demo.entity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import java.util.Set;
 
