4818e4bf0ff4a5fedc90ec7862fa623ce31557ba
bala-amypo <bala@amypo.in>
2025-12-28 11:45:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AssetDisposalResponse.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import java.time.LocalDate;
+
+public class AssetDisposalResponse {
+
+    private Long id;
+    private String disposalMethod;
+    private double disposalValue;
+    private LocalDate disposalDate;
+    private String assetStatus;
+    private String approvedBy;
+
+    // getters & setters
+}
