0f963819a5b5db740fb9a1497a66bf59a18a9e9b
bala-amypo <bala@amypo.in>
2025-12-28 11:00:23 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AssetLifecycleEventRequest.java
@@ -0,0 +1,41 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+import com.example.demo.entity.EventType;
+
+public class AssetLifecycleEventRequest {
+
+    @NotNull
+    private EventType eventType;
+
+    @NotNull
+    private LocalDate eventDate;
+
+    private String eventDescription;
+
+    // getters and setters
+    public EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public LocalDate getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(LocalDate eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getEventDescription() {
+        return eventDescription;
+    }
+
+    public void setEventDescription(String eventDescription) {
+        this.eventDescription = eventDescription;
+    }
+}
