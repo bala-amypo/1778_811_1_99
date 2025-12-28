42d983c80839ffc3246631942a8e76f11f9ac5c7
bala-amypo <bala@amypo.in>
2025-12-28 11:53:07 +0000

    User commit

--- src/main/java/com/example/demo/dto/AssetDisposalResponse.java
+++ src/main/java/com/example/demo/dto/AssetDisposalResponse.java
@@ -11,5 +11,53 @@ public class AssetDisposalResponse {
     private String assetStatus;
     private String approvedBy;
 
-    // getters & setters
+    // Getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getDisposalMethod() {
+        return disposalMethod;
+    }
+
+    public double getDisposalValue() {
+        return disposalValue;
+    }
+
+    public LocalDate getDisposalDate() {
+        return disposalDate;
+    }
+
+    public String getAssetStatus() {
+        return assetStatus;
+    }
+
+    public String getApprovedBy() {
+        return approvedBy;
+    }
+
+    // Setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDisposalMethod(String disposalMethod) {
+        this.disposalMethod = disposalMethod;
+    }
+
+    public void setDisposalValue(double disposalValue) {
+        this.disposalValue = disposalValue;
+    }
+
+    public void setDisposalDate(LocalDate disposalDate) {
+        this.disposalDate = disposalDate;
+    }
+
+    public void setAssetStatus(String assetStatus) {
+        this.assetStatus = assetStatus;
+    }
+
+    public void setApprovedBy(String approvedBy) {
+        this.approvedBy = approvedBy;
+    }
 }
